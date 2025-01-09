3369d8bc05273b2afed4e3e44f201e99cd9d09df
Kevin-gram <k.nyiringan@alustudent.com>
2025-01-09 14:06:05 +0200

    displaying the datas

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -13,7 +13,19 @@ sap.ui.define([
 
         _fetchBooks: function () {
             var oModel = this.getView().getModel();
-            oModel.loadData("/odata/v4/catalog/Books");
+            fetch("http://localhost:4004/odata/v4/catalog/Books")  // Direct link to the backend service
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok ' + response.statusText);
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    oModel.setData(data);
+                })
+                .catch(error => {
+                    console.error('There has been a problem with your fetch operation:', error);
+                });
         }
     });
 });
